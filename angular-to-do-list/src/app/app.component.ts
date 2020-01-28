41689799d37c13b17f57971043dbc4dfac1a1cfa
diksha-ganore <dikshaganore.mca@gmail.com>
2020-01-28 16:56:14 +0530

    ardhavat

--- angular-to-do-list/src/app/app.component.ts
+++ angular-to-do-list/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { toUnicode } from 'punycode';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-to-do-list';
+  title = 'My Todos';
+  todos = [
+  {
+    label: 'xyz',
+    done: false,
+    priority: 3
+  },
+  {
+    label: 'abc',
+    done: true,
+    priority: 1
+   },
+   {
+    label: 'pqr',
+    done: false,
+    priority: 4
+   },
+   {
+    label: 'stu',
+    done: false,
+    priority: 2
+   },
+  ];
+
+  addTodo(newTodoLabel: any){
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+  deleteTodo(todo: any) {
+    this.todos = this.todos.filter( t => t.label !== todo.label );
+  }
+  updateStatus(done: boolean,i: number) {
+    console.log(done,i)
+    todos
+    [i] == ;
+  }
 }
